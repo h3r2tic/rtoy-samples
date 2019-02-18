1cbec3389b190514a875d9bb6aae670a6fdbbf05
Tomasz Stachowiak <h3@h3.gd>
2019-02-18 04:09:12 +0100

    Fix buffer-test sample

--- src/bin/buffer-test/main.rs
+++ src/bin/buffer-test/main.rs
@@ -13,8 +13,8 @@ fn main() {
         tex_key,
         load_cs(asset!("shaders/buffer_test.glsl")),
         shader_uniforms!(
-            "buf0": upload_array_buffer(vec![0.04f32, 0.3f32, 0.8f32, 0.0f32]),
-            "buf1": upload_array_buffer(vec![0.5f32, 0.5f32]),
+            "buf0": upload_array_buffer(Box::new(vec![0.04f32, 0.3f32, 0.8f32, 0.0f32])),
+            "buf1": upload_array_buffer(Box::new(vec![0.5f32, 0.5f32])),
         ),
     );
 
