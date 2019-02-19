bc5680154eddf2c35d4a580ac414017c942dc01e
Tomasz Stachowiak <h3@h3.gd>
2019-02-19 03:13:32 +0100

    Reflection goodies

--- src/bin/hybrid-reflections/main.rs
+++ src/bin/hybrid-reflections/main.rs
@@ -1,3 +1,4 @@
+use rand::{distributions::StandardNormal, rngs::SmallRng, Rng, SeedableRng};
 use rendertoy::*;
 use rtoy_rt::*;
 
@@ -17,7 +18,6 @@ fn main() {
         format: gl::RGBA32F,
     };
 
-    //let scene_file = "assets/meshes/lighthouse.obj.gz";
     let scene_file = "assets/meshes/flying_trabant.obj.gz";
 
     let scene = load_obj_scene(scene_file.to_string());
@@ -40,7 +40,7 @@ fn main() {
         ),
     );
 
-    let shadowed_tex = compute_tex(
+    let reflected_tex = compute_tex(
         tex_key,
         load_cs(asset!("shaders/rt_hybrid_reflections.glsl")),
         shader_uniforms!(
@@ -51,13 +51,53 @@ fn main() {
         ),
     );
 
+    // We temporally accumulate raytraced images. The blend factor gets re-defined every frame.
+    let temporal_blend = init_dynamic!(const_f32(1f32));
+
+    // Need a valid value for the accumulation history. Black will do.
+    let accum_tex = init_dynamic!(load_tex(asset!("rendertoy::images/black.png")));
+
+    // Re-define the resource with a cycle upon itself -- every time it gets evaluated,
+    // it will use its previous value for "history", and produce a new value.
+    redef_dynamic!(
+        accum_tex,
+        compute_tex(
+            tex_key,
+            load_cs(asset!("shaders/blend.glsl")),
+            shader_uniforms!(
+                "inputTex1": accum_tex,
+                "inputTex2": reflected_tex,
+                "blendAmount": temporal_blend,
+            )
+        )
+    );
+
     let mut frame_idx = 0u32;
 
     rtoy.draw_forever(|frame_state| {
         camera.update(frame_state, 1.0 / 60.0);
 
-        let viewport_constants =
-            ViewportConstants::build(&camera, tex_key.width, tex_key.height).finish();
+        // If the camera is moving/rotating, reset image accumulation.
+        if !camera.is_converged() {
+            frame_idx = 0;
+        }
+
+        // Set the new blend factor such that we calculate a uniform average of all the traced frames.
+        redef_dynamic!(temporal_blend, const_f32(1.0 / (frame_idx as f32 + 1.0)));
+
+        // Jitter the image in a Gaussian kernel in order to anti-alias the result. This is why we have
+        // a post-process sharpen too. The Gaussian kernel eliminates jaggies, and then the post
+        // filter perceptually sharpens it whilst keeping the image alias-free.
+        let mut rng = SmallRng::seed_from_u64(frame_idx as u64);
+        let jitter = Vector2::new(
+            0.5 * rng.sample(StandardNormal) as f32,
+            0.5 * rng.sample(StandardNormal) as f32,
+        );
+
+        // Calculate the new viewport constants from the latest state
+        let viewport_constants = ViewportConstants::build(&camera, tex_key.width, tex_key.height)
+            .pixel_offset(jitter)
+            .finish();
 
         redef_dynamic!(
             constants_buf,
@@ -68,6 +108,6 @@ fn main() {
         );
 
         frame_idx += 1;
-        shadowed_tex
+        accum_tex
     });
 }
