8c0efd6813a6a22950a7f3895390f4c7415384d4
Tomasz Stachowiak <h3@h3.gd>
2019-11-05 21:54:49 +0100

    Simplify the ssao sample

--- src/bin/ssao/main.rs
+++ src/bin/ssao/main.rs
@@ -1,18 +1,14 @@
-use rand::{rngs::SmallRng, Rng, SeedableRng};
-use rand_distr::StandardNormal;
 use rendertoy::*;
 
 fn main() {
     let mut rtoy = Rendertoy::new();
     let tex_key = TextureKey::fullscreen(&rtoy, gl::RGBA32F);
 
-    let scene = load_gltf_scene(
-        //asset!("meshes/flying_trabant_final_takeoff/scene.gltf"),
-        asset!("meshes/the_lighthouse/scene.gltf"),
-        1.0,
-    );
+    let mesh = load_gltf_scene(asset!("meshes/the_lighthouse/scene.gltf"), 1.0);
+    let scene = vec![(mesh.clone(), Vector3::zeros(), UnitQuaternion::identity())];
 
-    let mut camera = FirstPersonCamera::new(Point3::new(0.0, 200.0, 800.0));
+    let mut camera =
+        CameraConvergenceEnforcer::new(FirstPersonCamera::new(Point3::new(0.0, 200.0, 800.0)));
 
     let mut raster_constants_buf = upload_buffer(0u32).into_named();
 
@@ -24,8 +20,7 @@ fn main() {
         ]),
         shader_uniforms!(
             constants: raster_constants_buf.clone(),
-            instance_transform: raster_mesh_transform(Vector3::zeros(), UnitQuaternion::identity()),
-            :upload_raster_mesh(make_raster_mesh(scene))
+            :upload_raster_scene(&scene)
         ),
     );
 
@@ -38,38 +33,15 @@ fn main() {
         .rgb = @input.rrr
     );
 
-    let out_tex = compute_tex(
-        tex_key,
-        load_cs(asset!("shaders/tonemap_sharpen.glsl")),
-        shader_uniforms!(
-            inputTex: out_tex,
-            sharpen_amount: 0.4f32,
-        ),
-    );
-
     let mut frame_idx = 0;
 
     rtoy.draw_forever(|frame_state| {
         camera.update(frame_state);
 
-        ssao.prepare_frame(
-            ViewConstants::build(&camera, tex_key.width, tex_key.height).finish(),
-            frame_idx,
-        );
-
-        // Jitter the image in a Gaussian kernel in order to anti-alias the result. This is why we have
-        // a post-process sharpen too. The Gaussian kernel eliminates jaggies, and then the post
-        // filter perceptually sharpens it whilst keeping the image alias-free.
-        let mut rng = SmallRng::seed_from_u64(frame_idx as u64);
-        let jitter = Vector2::new(
-            0.5 * rng.sample::<f32, _>(StandardNormal),
-            0.5 * rng.sample::<f32, _>(StandardNormal),
-        );
-
         // Calculate the new viewport constants from the latest state
-        let view_constants = ViewConstants::build(&camera, tex_key.width, tex_key.height)
-            .pixel_offset(jitter)
-            .finish();
+        let view_constants = ViewConstants::build(&camera, tex_key.width, tex_key.height).finish();
+
+        ssao.prepare_frame(&view_constants, &frame_state, frame_idx);
 
         raster_constants_buf.rebind(upload_buffer(view_constants));
 
